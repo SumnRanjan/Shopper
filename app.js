b86c582e4887c89cdfe519d61a1fe6fb7e0b8a6c
Suman Ranjan <sumanranjan17291729@gmail.com>
2024-11-21 20:28:11 +0530

    fg

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
